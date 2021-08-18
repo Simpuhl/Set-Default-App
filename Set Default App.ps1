4c41fc343d03815310a42fdc7532bf8e8b2311ca
Kevin Still <simpuhl@hotmail.com>
2021-08-18 11:01:43 -0700

    Update Set Default App.ps1

--- Set Default App.ps1
+++ Set Default App.ps1
@@ -2,7 +2,7 @@
 $file2 = 'C:\Program Files (x86)\Adobe\Acrobat Reader DC\Reader\AcroRd32.exe'
 
 
-if (-not(Test-Path -Path $file1 -PathType Leaf)) {
+if (Test-Path -Path $file1 -PathType Leaf) {
      try {
          Write-Host "Adobe Acrobat DC Found"
          Import-StartLayout -layoutpath C:\Default_Start_Layout1.xml -mountpath $Env:SYSTEMDRIVE\
@@ -14,7 +14,7 @@ if (-not(Test-Path -Path $file1 -PathType Leaf)) {
      }
  }
 
- if (-not(Test-Path -Path $file2 -PathType Leaf)) {
+ if (Test-Path -Path $file2 -PathType Leaf) {
      try {
          Write-Host "Adobe Acrobat Reader DC Found"
          Import-StartLayout -layoutpath C:\Default_Start_Layout2.xml -mountpath $Env:SYSTEMDRIVE\
